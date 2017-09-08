39df10a416a3db8bf36187c43fb7d7405171ebf1
Sailesh Bala <Sailesh Bala@DESKTOP-GITN1HN>
2017-09-07 23:09:02 -0400

    Buree

--- APCompSci/src/Lottery.java
+++ APCompSci/src/Lottery.java
@@ -1,4 +1,29 @@
 
-public class Lottery {
+import java.util.Random;
+public class Lottery{
+	public static void main(String[] args) {
+		Random rand = new Random();
+		String used = "";
+	
+		int count = 0;
+		while (count<=5){
+			int number = rand.nextInt(56) + 1;
+			if (used.contains("" + number)){
+				continue;
+			}
+			else{
+				used += "" + number  +",";
+				System.out.println(used);
+				count++;
+			}
+			
+		}
+		
+		int g = rand.nextInt(46) + 1;
+		System.out.println("Your First number from 1-46 is  " + " " + g);
+		
+
+		
+	}
 
 }
